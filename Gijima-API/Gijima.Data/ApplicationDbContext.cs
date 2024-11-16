57a4186f43b7b083d0ae5ff0035547cd942e5409
SphiwoluhleS <sphiwokuhleshandu24@gmail.com>
2024-11-16 15:54:14 +0200

    add offer migrations

--- Gijima-API/Gijima.Data/ApplicationDbContext.cs
+++ Gijima-API/Gijima.Data/ApplicationDbContext.cs
@@ -29,4 +29,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
     }
     public DbSet<Job> Jobs { get; set; }
     public DbSet<Area> Area { get; set; }
+    public DbSet<Offer> Offer { get; set; }
 }
--- Gijima-API/Gijima.Data/DataModels/Offer.cs
+++ Gijima-API/Gijima.Data/DataModels/Offer.cs
@@ -2,6 +2,11 @@ namespace Gijima.Data.DataModels;
 
 public class Offer : BaseDataModel
 {
-    public int price { get; set; }
-    public bool accepted { get; set; }
+    public string? UserId { get; set; }
+    public ApplicationUser? User { get; set; }
+    public int? JobId { get; set; }
+    public Job? Job { get; set; }
+    public int Price { get; set; } = 0;
+    public bool Accepted { get; set; } = false;
+    public string? Note { get; set; }
 }
