cdbe9419595ff6d946ffd1183eb5412d95363070
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-16 15:28:43 +0200

    Set up profile start.

--- Gijima-FE/src/components/Profile.jsx
+++ Gijima-FE/src/components/Profile.jsx
@@ -1,3 +1,17 @@
+import {useState} from "react";
 export default function Profile() {
-
+    const [user, setUser] = useState({
+        "id": 1,
+        "username": "john_doe",
+        "stars": 4,
+        "prefered_work": "Web Development",
+        "created_at": "2024-01-10T10:00:00Z"
+    }, [])
+    return (
+        <div>
+            <h1>{user.username}</h1>
+            <h2>Rating: {user.stars}/5</h2>
+            <h2>Prefered Work: {user.prefered_work}</h2>
+        </div>
+    )
 }
