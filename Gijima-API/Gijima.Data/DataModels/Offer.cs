46ecd399dfca9c0a9b7b9a4789607eaa7ff0eac8
SphiwoluhleS <sphiwokuhleshandu24@gmail.com>
2024-11-17 10:21:11 +0200

    update models

--- Gijima-API/Gijima.Data/DataModels/Offer.cs
+++ Gijima-API/Gijima.Data/DataModels/Offer.cs
@@ -7,6 +7,7 @@ public class Offer : BaseDataModel
     [JsonPropertyName("user_id")]
     public string? UserId { get; set; }
     public ApplicationUser? User { get; set; }
+    [JsonPropertyName("job_id")]
     public int? JobId { get; set; }
     public Job? Job { get; set; }
     public int Price { get; set; } = 0;
