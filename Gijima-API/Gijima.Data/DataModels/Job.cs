486a472d37c36fa65d28c24de98d890799f8bf87
SphiwoluhleS <sphiwokuhleshandu24@gmail.com>
2024-11-17 09:33:24 +0200

    add json tags

--- Gijima-API/Gijima.Data/DataModels/Job.cs
+++ Gijima-API/Gijima.Data/DataModels/Job.cs
@@ -1,15 +1,23 @@
 namespace Gijima.Data.DataModels;
+using System.Text.Json.Serialization;
 
 public class Job : BaseDataModel
 {
+                [JsonPropertyName("user_id")]
                 public string UserId { get; set; }
                 public ApplicationUser ?  User { get; set; }
+                [JsonPropertyName("provider_id")]
                 public string ? ProviderId { get; set; } 
                 public ApplicationUser ? Provider { get; set; }
+                [JsonPropertyName("area_id")]
                 public int AreaId { get; set; }
                 public Area? Area { get; set; }
+                [JsonPropertyName("title")]
                 public string Title { get; set; }
-                public string ?  Description {get; set;} 
+                [JsonPropertyName("description")]
+                public string ?  Description {get; set;}
+                [JsonPropertyName("price")]
                 public int Price {get; set;}
+                [JsonPropertyName("active")]
                 public bool Active {get; set;}
 }   
