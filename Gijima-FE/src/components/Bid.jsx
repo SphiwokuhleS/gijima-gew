6ba7279b2b7abbe05398bc8b49ed440b9191330c
Dylan Ranger <dylan.ranger.dev@gmail.com>
2024-11-17 09:36:42 +0200

    Fix mutation. Update bid

--- Gijima-FE/src/components/Bid.jsx
+++ Gijima-FE/src/components/Bid.jsx
@@ -2,6 +2,7 @@ import Tile from "./layout/Tile.jsx";
 import { useNavigate } from 'react-router-dom';
 
 export default function Bid({bid}) {
+    console.log(bid)
     const jobId = parseInt(bid.jobId);
     const navigate = useNavigate();
     function goToJobDetail(id) {
@@ -9,9 +10,9 @@ export default function Bid({bid}) {
     }
     return (
         <Tile classes="bg-gray-200" onClick={() => goToJobDetail(jobId)}>
-            <h2>Bid Number {bid.id}</h2>
-            <p>{bid.note}</p>
-            <p>{bid.price}</p>
+            <h2 className="text-lg text-semibold">Bid Number {bid.id}</h2>
+            <p>{bid.description}</p>
+            <p>R{bid.amount}</p>
         </Tile>
     )
 }
